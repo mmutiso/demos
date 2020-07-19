252aba5decd335e0b96f5128137c29b82028d08a
Matthew Mutiso <mutee88@gmail.com>
2020-07-19 21:59:33 +0300

    Added visual studio gitignore

---
+++ decoded-lambda-demo/LambdaFunctionEntry.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace decoded_lambda_demo
+{
+    public class LambdaFunctionEntry
+    {
+        public async Task FunctionHandler(SQSEvent invocationEvent, ILambdaContext context)
+        {
+            await Task.FromResult(0); // dummy awaiter
+
+            LambdaLogger.Log(JsonSerializer.Serialize(invocationEvent));
+        }
+    }
+}
